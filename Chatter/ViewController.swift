d35a9b6f10dcf64e20d564750866e7bd9efa1c11
Tanmay Bakshi <tanmay@tanmays-imac.local>
2016-09-17 00:36:48 -0400

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Tanmay Bakshi on 2016-09-17.
+//  Copyright © 2016 Tanmay Bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
